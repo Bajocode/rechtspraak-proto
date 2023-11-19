19d647e1e233a3a741d57350cabf0de30ef1a763
Fabijan Bajo <bajo09@gmail.com>
2023-11-19 17:02:04 +0100

    base

---
+++ components/nav.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+
+export function Nav() {
+  return (
+    <div className="flex gap-6 md:gap-10">
+      <Link href="/" className="hidden items-center space-x-2 md:flex">
+        <span className="hidden font-bold sm:inline-block">
+          Baseline
+        </span>
+      </Link>
+        <nav className="hidden gap-6 md:flex">
+          <Link
+            href="#"
+            className={cn(
+              "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
+              "text-foreground")} 
+          >
+            Linkie
+          </Link>
+        </nav>
+    </div>
+  )
+}
