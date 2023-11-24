121a0ed1bfcb95fa4d339c9e86a69f54e4d27911
Fabijan Bajo <bajo09@gmail.com>
2023-11-24 21:20:59 +0100

    basic chat endpoint

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,22 +10,32 @@ const openai = new OpenAI({
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
-  const { messages } = await req.json();
+  try {
+    // Extract the `prompt` from the body of the request
+    const { messages } = await req.json();
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const systemMessage: Message = {
-    role: 'system',
-    content: 'Je bent een behulpzame jurisprudentie assistent die  vragen beantwoord over rechtspraken, en met behulp van uitspraak documenten informatie kan opzoeken op onderwerp, zaaknummer, ECLI, maar ook vragen over recht en uitspraken in het algemeen'
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const prompt = [
+      {
+        role: 'system',
+        content: `Je bent een behulpzame jurisprudentie assistent.
+      Je beantwoord vragen beantwoord over rechtspraken met behulp van meegestuurde rechtspraak documenten.
+      `
+      }
+    ]
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages: [
+        ...prompt, 
+        ...messages],
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (e) {
+    throw e;
   }
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: [systemMessage, ...messages],
-  });
-
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
 }
